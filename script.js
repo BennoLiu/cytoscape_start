87cb7ae4dee0a3b823bbd682513481c8922d81b9
Benno Liu <benno.liu@sunbirddcim.com>
2022-11-29 15:09:43 +0800

    refactor

--- script.js
+++ script.js
@@ -6,11 +6,33 @@ import { mesh1, tier1 } from './data';
 cytoscape.use(fcose);
 cytoscape.use(dagre);
 
-const cy = cytoscape({
-  container: document.querySelector('#cy'),
-  elements: tier1,
-  // elements: mesh1,
-  style: [
+let cy;
+
+initMesh();
+
+document.querySelector('#initMesh').addEventListener('click', initMesh);
+document.querySelector('#initTier').addEventListener('click', initTier);
+
+function initMesh() {
+  const meshStyle = shareStyle();
+  const meshLayout = {
+    name: 'fcose',
+    nodeDimensionsIncludeLabels: true,
+  };
+  init(mesh1, meshStyle, meshLayout);
+}
+
+function initTier() {
+  const tierStyle = shareStyle();
+  const tierLayout = {
+    name: 'dagre',
+    nodeDimensionsIncludeLabels: true,
+  };
+  init(tier1, tierStyle, tierLayout);
+}
+
+function shareStyle() {
+  return [
     {
       selector: 'node',
       style: {
@@ -29,35 +51,28 @@ const cy = cytoscape({
         width: 4,
       },
     },
-  ],
-  layout: {
-    name: 'dagre',
-    // name: 'fcose',
-    nodeDimensionsIncludeLabels: true,
-  },
-  wheelSensitivity: 0.1,
-});
+  ];
+}
 
-document.querySelector('#btnAddEvents').addEventListener('click', () => {
+function init(elements, style, layout) {
+  cy = cytoscape({
+    container: document.querySelector('#cy'),
+    elements: elements,
+    style: style,
+    layout: layout,
+    wheelSensitivity: 0.1,
+  });
   addEvents();
-});
-document.querySelector('#btnRemoveEvents').addEventListener('click', () => {
-  removeEvents();
-});
+}
 
 function addEvents() {
   cy.edges().on('mousemove', edgeHovered).on('mouseout', edgeHoveredOut);
 }
-function removeEvents() {
-  cy.edges().removeListener('mousemove', edgeHovered).removeListener('mouseout', edgeHoveredOut);
-}
-
 function edgeHovered(evt) {
   const edge = evt.target;
   edge.addClass('hovered');
 }
 function edgeHoveredOut(evt) {
-  console.log('hovered out');
   const edge = evt.target;
   edge.removeClass('hovered');
 }
